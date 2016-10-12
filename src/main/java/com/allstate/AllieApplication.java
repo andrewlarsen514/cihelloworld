0c1ebfa1b62f37e3468e4a82581ad7715cac3141
andrewlarsen514 <andrew.larse514@gmail.com>
2016-10-12 12:07:48 -0500

    updating to war file for build

--- src/main/java/com/allstate/AllieApplication.java
+++ src/main/java/com/allstate/AllieApplication.java
@@ -2,11 +2,20 @@ package com.allstate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AllieApplication {
+public class AllieApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AllieApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<AllieApplication> applicationClass = AllieApplication.class;
 }
